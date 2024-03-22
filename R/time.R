95ccffbfc6654859fc6dc4831fa6b2ea9e74db29
Sunny Tseng <sunnyyctseng@gmail.com>
2024-03-22 13:51:12 -0700

    finished the second training

--- R/time.R
+++ R/time.R
@@ -11,15 +11,16 @@
 #' what_time(language = "en")
 what_time <- function(language = "fr") {
 
-  if (!language %in% c("fr", "en")) {
-    stop("Either choose 'fr' or 'en' as a language.")
-  }
+  rlang::arg_match0(language, c("fr", "en"))
 
   time <- format(Sys.time(), "%H:%M")
 
+  exclamation <- praise::praise("${Exclamation}")
+
   switch(
     language,
-    fr = sprintf("Il est maintenant %s!", time),
-    en = sprintf("It is %s now!", time)
+    fr = sprintf("%s! Il est maintenant %s!", exclamation, time),
+    en = sprintf("%s! It is %s now!", exclamation, time)
   )
 }
+
